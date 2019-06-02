5b9484d4d1fedded1d79dba82ca9fb7126e7d322
piotrek81201 <48654395+piotrek81201@users.noreply.github.com>
2019-06-02 18:59:21 +0200

    update

--- GraZaDuzoZaMalo/ModelGry/Gra.Historia.cs
+++ GraZaDuzoZaMalo/ModelGry/Gra.Historia.cs
@@ -1,10 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ModelGry
 {
-    class Class1
+    public partial class Gra
     {
+        private readonly List<Ruch> historia;
+        public IReadOnlyList<Ruch> Historia => historia;
+
+        public class Ruch
+        {
+            public readonly int propozycja;
+            public readonly Odpowiedz odpowiedz;
+            public readonly DateTime kiedy;
+
+            internal Ruch(int prop, Odpowiedz odp)
+            {
+                propozycja = prop;
+                odpowiedz = odp;
+                kiedy = DateTime.Now;
+            }
+
+            public override string ToString() =>
+                $"( {propozycja}, {odpowiedz}, {kiedy})";
+        }
     }
 }
